ffe8829d2dfaecefa165e0b30044425f422ecc39
DmitryBurnaev <dmitry.burnaev@gmail.com>
2023-12-31 16:58:25 +0300

    [#16] Encrypt result backup: extended using click library

--- src/run.py
+++ src/run.py
@@ -5,16 +5,15 @@ You can get additional information by using:
 $ python3 -m src.run --help
 
 """
-import click
+import os
+import sys
 import logging
 from logging import config
 
+import click
 import sentry_sdk
 
 from src import settings
-from src.backup import run_backup
-from src.handlers import HANDLERS
-from src.utils import colorized_echo
 
 logging.config.dictConfig(settings.LOGGING)
 logger = logging.getLogger(__name__)
@@ -28,85 +27,6 @@ ENCRYPTION_PASS = {
 
 if settings.SENTRY_DSN:
     sentry_sdk.init(settings.SENTRY_DSN)
-
-#
-# @click.command()
-# @click.argument(
-#     "db",
-#     metavar="DB_NAME",
-#     type=str,
-# )
-# @click.option(
-#     "-h", "--handler",
-#     metavar="BACKUP_HANDLER",
-#     type=str,
-#     required=True,
-#     show_choices=HANDLERS.keys(),
-#     help=f"Handler, that will be used for backup {tuple(HANDLERS.keys())}",
-# )
-# @click.option(
-#     "-dc", "--docker-container",
-#     metavar="CONTAINER_NAME",
-#     type=str,
-#     help="""
-#         Name of docker container which should be used for getting dump.
-#         Required for using docker_* handler
-#     """,
-# )
-# @click.option(
-#     "-e", "--encrypt",
-#     is_flag=True,
-#     flag_value=True,
-#     help="Turn ON backup's encryption (with openssl)",
-# )
-# @click.option(
-#     "--encrypt-pass",
-#     type=str,
-#     metavar="DB_BACKUP_ENCRYPT_PASS",
-#     default="env:DB_BACKUP_ENCRYPT_PASS",
-#     show_default=True,
-#     help=f"""
-#         Openssl config to provide source of encryption pass: {tuple(ENCRYPTION_PASS.keys())} |
-#         see details in README.md
-#     """,
-# )
-# @click.option(
-#     "--s3",
-#     is_flag=True,
-#     flag_value=True,
-#     help="Send backup to S3-like storage (requires DB_BACKUP_S3_* env vars)",
-# )
-# @click.option(
-#     "-l", "--local",
-#     is_flag=True,
-#     flag_value=True,
-#     help="Store backup locally (requires DB_BACKUP_LOCAL_PATH env)",
-# )
-# def backup(
-#     handler: str,
-#     db: str,
-#     docker_container: str | None,
-#     encrypt: bool,
-#     encrypt_pass: str | None,
-#     s3: bool,
-#     local: bool
-# ):
-#     """Simple program that backups db 'DB_NAME' via specific BACKUP_HANDLER."""
-#     colorized_echo(handler, db)
-#     run_backup(
-#         handler=handler,
-#         db=db,
-#         docker_container=docker_container,
-#         encrypt=encrypt,
-#         encrypt_pass=encrypt_pass,
-#         local=local,
-#         s3=s3,
-#     )
-#
-
-import os
-import sys
-import click
 
 
 CONTEXT_SETTINGS = dict(auto_envvar_prefix="COMPLEX")
@@ -157,7 +77,7 @@ class ComplexCLI(click.Group):
     def list_commands(self, ctx):
         rv = []
         for filename in os.listdir(self.cmd_folder):
-            if filename.endswith(".py"):
+            if filename.endswith(".py") and not filename.startswith("__"):
                 rv.append(filename.replace(".py", ""))
 
         rv.sort()
